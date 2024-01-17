adec53b2c919b60fc644010691c62eb1923b5df3
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-17 16:46:39 +0100

    extensions component created and routed in the app component

--- bookmark-app/src/app/app.component.ts
+++ bookmark-app/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 import { FeaturesComponent } from './features/features.component';
+import { ExtensionsComponent } from './extensions/extensions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent, HomeComponent, FeaturesComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, HomeComponent, FeaturesComponent, ExtensionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ bookmark-app/src/app/extensions/extensions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extensions',
+  standalone: true,
+  imports: [],
+  templateUrl: './extensions.component.html',
+  styleUrl: './extensions.component.css'
+})
+export class ExtensionsComponent {
+
+}
