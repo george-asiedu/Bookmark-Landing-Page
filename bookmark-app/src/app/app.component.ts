c1141b90fbe98c501a45ade04f7cd120cf79abdc
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-17 15:14:11 +0100

    features component text context added

--- bookmark-app/src/app/app.component.ts
+++ bookmark-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, HomeComponent, FeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
