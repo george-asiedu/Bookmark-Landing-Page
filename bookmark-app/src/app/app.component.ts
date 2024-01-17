55d2265d21eaef228b0b556c0008f1db6e9cdce2
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-17 11:06:08 +0100

    home component created

--- bookmark-app/src/app/app.component.ts
+++ bookmark-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
