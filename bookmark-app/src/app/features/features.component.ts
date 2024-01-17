273e2dfe3e9f8bae7ccf458e04d503f1da4bc52e
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-17 15:55:30 +0100

    features tab features addeded

--- bookmark-app/src/app/features/features.component.ts
+++ bookmark-app/src/app/features/features.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './features.component.html',
   styleUrl: './features.component.css'
 })
