176b579be03f3b95e9a5b35254ffc4d40bbf55ec
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-19 12:44:39 +0100

    faq component header text added

--- bookmark-app/src/app/app.component.ts
+++ bookmark-app/src/app/app.component.ts
@@ -5,11 +5,20 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 import { FeaturesComponent } from './features/features.component';
 import { ExtensionsComponent } from './extensions/extensions.component';
+import { FAQComponent } from './faq/faq.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent, HomeComponent, FeaturesComponent, ExtensionsComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    NavigationComponent, 
+    HomeComponent, 
+    FeaturesComponent, 
+    ExtensionsComponent,
+    FAQComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
